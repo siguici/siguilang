a1bf47915ff57a471cc74180e60d236d6eb4e8ed
⚡ Sigui 🇨🇮 <siguici@proton.me>
2025-10-18 01:51:51 +0100

    💡 Improve examples

--- src/ske_test.v
+++ src/ske_test.v
@@ -7,5 +7,5 @@ const vexe = os.real_path(os.getenv_opt('VEXE') or { @VEXE })
 fn test_examples() {
 	r := os.execute('${vexe} run . examples/test')
 	assert r.exit_code == 0
-	assert r.output == 'Hello World!\n'
+	assert r.output.ends_with('Enjoy Ske!\n')
 }
